beb7c0f1a8f035a68f1d5dcc9baa906d2fbc7559
walteribanez555 <walteribanez555@gmail.com>
2024-09-03 11:07:36 -0400

    Model Discount updated

--- src/models/discounts.model.mjs
+++ src/models/discounts.model.mjs
@@ -1,54 +1,91 @@
 import { DatabaseOperations } from "../utils/database.mjs";
 import { buildResponse, validateData, colorLog } from "../utils/helpers.mjs";
 
+const tableName = "discounts";
+const idField = "discount_id";
+const keyField = "discount_id";
 
-const tableName = 'discounts';
-const idField = 'discount_id';
-const keyField = 'discount_id';
+const model = {
+  title: "string",
+  description: "string",
+  amount: "number",
+  type: "number",
+  imagesUrl: "string",
+};
 
-const model ={
-    title : 'string',
-    description : 'string',
-    amount : 'number',
-    type : 'number',
-    imagesUrl : 'string',
+export async function getDiscount({ id, schema }) {
+  try {
+    const database = new DatabaseOperations(tableName, schema);
+    const data = {
+      where: {
+        [keyField]: id,
+      },
+    };
+    const response = await database.read(data);
+
+    return buildResponse(200, response, "get");
+  } catch (err) {
+    colorLog(` GET DISCOUNT ERROR:  ${JSON.stringify(error)}`, "red", "reset");
+    return buildResponse(500, error, "get");
+  }
 }
 
+export async function postDiscount({ data, schema }) {
+  try {
+    const database = new DatabaseOperations(tableName, schema);
+    const newRegister = validateData(data, model);
+    if (Object.keys(newRegister).length === 0)
+      return buildResponse(
+        400,
+        { message: "Missing required fields or not valid" },
+        "post"
+      );
 
-
-export async function getDiscount({id, schema}){
-
-    try{
-
-    }catch( err ) {
-        
-    }
+    const response = await database.create(newRegister, keyField);
+    return buildResponse(200, response, "post", keyField, data);
+  } catch (err) {
+    colorLog(` POST DISCOUNT ERROR:  ${JSON.stringify(error)}`, "red", "reset");
+    return buildResponse(500, error, "post");
+  }
 }
 
+export async function putDiscount({ id, data, schema }) {
+  try {
+    const database = new DatabaseOperations(tableName, schema);
+    const update = validateData(data, model, 'put');
 
-export async function postDiscount({data, schema}){
+    if(Object.keys( update ).length === 0)
+        return buildResponse(400, {message : 'Missing fields to update'}, 'put');
 
-    try{
+    if( !id )
+        return buildResponse(400, { message : 'Missing the record id to update'});
 
-    }catch(err){
+    const where = {
+        [keyField] : id
+    };
 
-    }
+    const response = await database.update(update, where);
+    return buildResponse(200, response, 'put');
+  } catch (err) {
+    colorLog(` PUT DISCOUNT ERROR:  ${JSON.stringify(error)}`, "red", "reset");
+    return buildResponse(500, error, "put");
+  }
 }
 
+export async function deleteDiscount({ id, schema }) {
+  try {
+    const database = new DatabaseOperations(tableName, schema);
 
-export async function putDiscount({id, data ,schema}){
-    try{
+    if(!id)
+        return buildResponse(400, { message : 'Missing the record id to delete'});
 
-    }catch( err ){
+    await database.delete(id, keyField);
+    return buildResponse(200, {message: 'Record deleted'}, 'delete');
 
-    }
 
-}
+  } catch (err) {
+    colorLog(` DELETE DISCOUNT ERROR:  ${JSON.stringify(error)}`, "red", "reset");
+    return buildResponse(500, error, "delete");
 
-export async function deleteDiscount({id, schema}){ 
-    try{
-
-    }catch(err) { 
-         
-    }
+  }
 }
